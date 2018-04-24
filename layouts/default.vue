1dfeb18f4a014fb9d692e280ee32ef6f53ecf9a2
Lavoie-Giasson Laurier <1586848@decinfo.cvm>
2018-04-24 15:13:36 -0400

    edit index.vue pour le login, on fait mtn le lien avec typepartie.vue de loic

--- layouts/default.vue
+++ layouts/default.vue
@@ -88,7 +88,8 @@
         fixed: false,
         items: [
           { icon: 'apps', title: 'Welcome', to: '/' },
-          { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
+          { icon: 'bubble_chart', title: 'Carom 1', to: '/inspire' },
+          { icon: 'bubble_chart', title: 'Carom 2', to: '/carom' }
         ],
         miniVariant: false,
         right: true,
