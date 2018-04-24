2aca39ae5a08acf3ddeac5896a5128c28d12eec0
Zénon Roy <king-zed@hotmail.com>
2018-04-24 09:57:20 -0400

    users

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
         <img src="/v.png" alt="Vuetify.js" class="mb-5" />
       </div>
       <v-card>
-        <v-card-title class="headline">Welcome to the Vuetify + Nuxt.js template</v-card-title>
+        <v-card-title class="headline">Welcome to the colgate + Nuxt.js template</v-card-title>
         <v-card-text>
           <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
           <p>For more information on Vuetify, check out the <a href="https://vuetifyjs.com" target="_blank">documentation</a>.</p>
--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
 const parties = require('./parties/parties.service.js');
+const users = require('./users/users.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(parties);
+  app.configure(users);
 };
