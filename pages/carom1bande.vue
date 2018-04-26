0c6ac03d192268fb446443df43c530a0fc2250f6
Loic <bachellerieloic@gmail.com>
2018-04-26 12:44:42 -0400

    Pages Parties Différentes

--- pages/carom1bande.vue
+++ pages/carom1bande.vue
@@ -1,11 +1,11 @@
 <template>
 	<div>
 		<div class="text-xs-center">
-      <h1>CAROM LIBRE</h1>
-    </div>
+			<h1>CAROM À UNE BANDE</h1>
+			</div>
 		<div class="">
 			<nuxt-link to="/typepartie">
-        <v-btn round color="primary" dark>RETOUR</v-btn>
+				<v-btn round color="primary" dark>RETOUR</v-btn>
 			</nuxt-link>  
 		</div>
 	</div>
