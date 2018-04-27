f31f88f33ed01014f870171ee5b3bdd75d994b24
Zenon Roy <king-zed@hotmail.com>
2018-04-27 19:17:19 -0400

    démarre carom automatiquement

--- pages/carom1bande.vue
+++ pages/carom1bande.vue
@@ -1,8 +1,8 @@
 <template>
-	<div>
-		<div class="text-xs-center">
+	<v-layout>
+		<v-flex >
 			<h1>CAROM À UNE BANDE</h1>
-			</div>
+		</v-flex>
 		<div class="">
 			<nuxt-link to="/typepartie">
 				<v-btn round color="primary" dark>RETOUR</v-btn>
@@ -10,17 +10,17 @@
 			<v-btn round color="primary" v-on:click="start()" dark>PARTIE</v-btn>
 			<div id="carom-container" style="position:fixed;top:20%;left:0px; border:2px solid; height:76%;width:100%;"></div>
 		</div>
-	</div>
+	</v-layout>
 </template>
 <script>
 import carom from 'assets/js/Carom.js';
 export default {
-	created () {
-		//carom.demarrerPartie();
+	mounted () {
+		carom.demarrerPartie();
 	},
 	methods: {
 		start(){
-			carom.demarrerPartie();
+			// carom.demarrerPartie();
 		}
 	}
 }
