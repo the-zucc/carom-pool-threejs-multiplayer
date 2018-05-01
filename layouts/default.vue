f165b764c8afacf83efeb8f675df2506937270ce
Loic <bachellerieloic@gmail.com>
2018-05-01 12:52:02 -0400

    Drawer Stats

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,50 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer app fixed :clipped="$vuetify.breakpoint.lgAndUp" v-model="drawer"></v-navigation-drawer>
+    <v-navigation-drawer app fixed :clipped="$vuetify.breakpoint.lgAndUp" v-model="drawer">
+        <v-list class="pa-1">
+        <v-list-tile v-if="mini" @click.stop="mini = !mini">
+          <v-list-tile-action>
+            <v-icon>chevron_right</v-icon>
+          </v-list-tile-action>
+        </v-list-tile>
+        <v-list-tile avatar tag="div">
+          <v-list-tile-avatar>
+            <img src="https://randomuser.me/api/portraits/men/85.jpg" >
+          </v-list-tile-avatar>
+          <v-list-tile-content>
+            <v-list-tile-title>JM DESCHAMPS</v-list-tile-title>
+          </v-list-tile-content>
+          <v-list-tile-action>
+            <v-btn icon @click.stop="drawer = !drawer">
+              <v-icon>chevron_left</v-icon>
+            </v-btn>
+          </v-list-tile-action>
+        </v-list-tile>
+      </v-list>
+
+       <v-list class="pt-0" dense>
+        <v-divider></v-divider>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>accessibility</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Statistiques</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+
+        <v-divider></v-divider>
+        <v-list-tile v-for="item in items" :key="item.title" @click="">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }} : {{item.number}}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+
+    </v-navigation-drawer>
     <v-toolbar app :clipped-left="$vuetify.breakpoint.lgAndUp" fixed color="primary" dark>
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
        <i class="material-icons">bubble_chart</i>
@@ -28,7 +72,10 @@ export default {
       clipped: true,
       drawer: true,
       fixed: false,
-      items: [{ icon: "apps", title: "Welcome", to: "/" }],
+      items: [
+        { title: 'Nombre de coups', number : '10', icon: 'bubble_chart' },
+        { title: 'Nombre de carom', number : '1', icon: 'timeline' }
+      ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
