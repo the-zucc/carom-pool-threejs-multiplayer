644b12d7b2d94ccefc76ff2dfef5b3d682af4536
Loic <bachellerieloic@gmail.com>
2018-05-01 13:47:32 -0400

    modifs envoyees pour que laurier remplisse le layout

--- pages/carom/_id.vue
+++ pages/carom/_id.vue
@@ -4,10 +4,9 @@
 			<h1>CAROM À UNE BANDE</h1>
 		</v-flex>
 		<div class="">
-			<nuxt-link to="/typepartie">
+			<nuxt-link to="/">
 				<v-btn round color="primary" dark>RETOUR</v-btn>
 			</nuxt-link>  
-			<v-btn round color="primary" v-on:click="start()" dark>PARTIE</v-btn>
 			<div id="carom-container" style="position:fixed;top:20%;left:0px; border:2px solid; height:76%;width:100%;"></div>
 		</div>
 	</v-layout>
@@ -17,11 +16,6 @@ import carom from 'assets/js/Carom.js';
 export default {
 	mounted () {
 		carom.demarrerPartie();
-	},
-	methods: {
-		start(){
-			// carom.demarrerPartie();
-		}
 	}
 }
 </script>
