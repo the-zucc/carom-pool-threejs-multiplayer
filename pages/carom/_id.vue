8bff442448977162649a6d37a71078afe6e694d8
Zénon Roy <king-zed@hotmail.com>
2018-05-01 13:51:35 -0400

    layout dans jeu

--- pages/carom/_id.vue
+++ pages/carom/_id.vue
@@ -1,15 +1,6 @@
 <template>
 	<v-layout>
-		<v-flex >
-			<h1>CAROM À UNE BANDE</h1>
-		</v-flex>
-		<div class="">
-			<nuxt-link to="/typepartie">
-				<v-btn round color="primary" dark>RETOUR</v-btn>
-			</nuxt-link>  
-			<v-btn round color="primary" v-on:click="start()" dark>PARTIE</v-btn>
-			<div id="carom-container" style="position:fixed;top:20%;left:0px; border:2px solid; height:76%;width:100%;"></div>
-		</div>
+		<v-flex id="carom-container"></v-flex>
 	</v-layout>
 </template>
 <script>
